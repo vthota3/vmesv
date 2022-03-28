c3dc9ff722c6483a34895cfbd9ebfb9721def6e6
Venkata pruthvi Thota <thota.venkatapruthvi6@gmail.com>
2022-03-27 22:05:09 -0700

    changes

--- main.py
+++ main.py
@@ -10,33 +10,33 @@ import argparse
 import yaml
 
 class main:
-    
+    ## Function Name: __init__
+    ## Description: constructor function
     def __init__(self):
         #RM_PROD print("In the INIT method")
-        self.get_variables()
+        self.get_args()
 
-
+    ## Function Name: yaml_loader
+    ## Description: This function is used to convert the input yaml file into dictoniary 
     def yaml_loader(self,filepath):
         with open(filepath,"r") as file_descriptor:
             data = yaml.safe_load(file_descriptor)
         print(data)
         return data
 
-
-    def get_variables(self):
+    ## Function Name: get_args
+    ## Description: This function is used to get parse the args
+    def get_args(self):
         parser = argparse.ArgumentParser()
         parser.add_argument('--file',type=str,required=True)
         args = parser.parse_args()
         print("File Provided by user: "+args.file)
         self.yaml_loader(args.file)
 
-    def print_obj(self):
-        print("Main Object") 
 
 
 if __name__ == '__main__':
     main_obj = main()
-    main_obj.print_obj()
 
 
 
